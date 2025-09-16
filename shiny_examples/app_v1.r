326e8f8540c958a182622a55841a3bba1962227a
Joao <jg.gabrielborges@gmail.com>
2025-09-16 08:49:15 -0300

    added README and shiny examples

---
+++ shiny_examples/app_v1.r
@@ -0,0 +1,20 @@
+library(shiny)
+
+
+# example <- "01_hello"
+# example <- "02_text"
+# example <- "03_reactivity"
+# example <- "04_mpg"
+# example <- "05_sliders"
+example <- "06_tabsets"
+# example <- "07_widgets"
+# example <- "08_html"
+# example <- "09_upload"
+# example <- "10_download"
+# example <- "11_timer"
+
+
+
+
+
+runExample(example)
